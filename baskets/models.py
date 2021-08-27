2a88974e2a1d4147a53e1ac797cd749d1978e9ab
Hovo-93 <ovo.aroyan@mail.ru>
2021-08-27 16:58:31 +0400

    added @cache_property

--- baskets/models.py
+++ baskets/models.py
@@ -16,26 +16,26 @@ class Basket(models.Model):
 
     def summa(self):
         return self.quantity * self.product.price
-    # @property
-    # def product_cost(self):
-    #     return self.user.product.price * self.quantity
+    @property
+    def product_cost(self):
+        return self.product.price * self.quantity
 
 
-    def total_quantity(self):
-        baskets = Basket.objects.filter(user=self.user)
-        return sum(basket.quantity for basket in baskets)
-
-    def total_sum(self):
-        baskets = Basket.objects.filter(user=self.user)
-        return sum(basket.summa() for basket in baskets)
-    # @cached_property
-    # def get_items_cached(self):
-    #     return self.baskets.select_related()
+    # def total_quantity(self):
+    #     baskets = Basket.objects.filter(user=self.user)
+    #     return sum(basket.quantity for basket in baskets)
     #
-    # def get_total_quantity(self):
-    #     _items = self.get_items_cached
-    #     return sum(list(map(lambda x: x.quantity, _items)))
-    #
-    # def get_total_cost(self):
-    #     _items = self.get_items_cached
-    #     return sum(list(map(lambda x: x.product_cost, _items)))
+    # def total_sum(self):
+    #     baskets = Basket.objects.filter(user=self.user)
+    #     return sum(basket.summa() for basket in baskets)
+    @cached_property
+    def get_items_cached(self):
+        return self.baskets.select_related()
+
+    def get_total_quantity(self):
+        _items = self.get_items_cached
+        return sum(list(map(lambda x: x.quantity, _items)))
+
+    def get_total_cost(self):
+        _items = self.get_items_cached
+        return sum(list(map(lambda x: x.product_cost, _items)))
