a6b167eff164836b73741adf6dbff0b3a294d848
Hovo-93 <ovo.aroyan@mail.ru>
2021-09-05 12:18:22 +0300

    les8 testCase user

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,40 @@
 from django.test import TestCase
+from django.test import Client
 
 # Create your tests here.
+from users.models import UserProfile
+
+
+class UserAuthTestCase(TestCase):
+    status_code_success = 200
+    status_code_redirect = 302
+    status_code_forbidden = 403
+    username = 'django_test2'
+    user_password = 'geekbrains'
+
+    def setUp(self):
+        self.client = Client()
+        self.user = UserProfile.objects.create(
+            username=self.username,
+            password=self.user_password,
+            email='django_test2@gb.local',
+        )
+
+    def test_login_user(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, self.status_code_success)
+        self.assertTrue(response.context['user'].is_anonymous)
+
+        # self.client.login(username = self.username,password=self.user_password)
+        user_data ={
+            'username':self.username,
+            'password':self.user_password
+        }
+
+        response = self.client.get('/user/login/',data=user_data)
+        self.assertEqual(response.status_code, self.status_code_redirect)
+
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, self.status_code_success)
+        self.assertFalse(response.context['user'].is_anonymous)
+
