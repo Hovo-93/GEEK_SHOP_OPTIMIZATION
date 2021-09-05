f297d60c9f2ec605e3c91fffd69fc36f1c82912c
Hovo-93 <ovo.aroyan@mail.ru>
2021-09-05 11:46:02 +0300

    les8 testCase prod

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from django.test import Client
 
-# Create your tests here.
+from products.models import ProductCategory, Product
+
+
+class ProductsAppSmokeTest(TestCase):
+    status_code_success = 200
+    category = ProductCategory.objects.create(
+        name ='cat1'
+    )
+    for i in range(10):
+        Product.objects.create(
+            category = category,
+            name =f'prod{i}',
+        )
+    def setUp(self):
+        self.client = Client()
+
+    def test_product_app_urls(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, self.status_code_success)
+
+        response = self.client.get('/products/')
+        self.assertEqual(response.status_code, self.status_code_success)
+
+    def test_products_list(self):
+        for product_item in Product.objects.all():
+            response = self.client.get(f'/products/{product_item.pk}/')
+            self.assertEqual(response.status_code,self.status_code_success)
+
+
+
+
+
